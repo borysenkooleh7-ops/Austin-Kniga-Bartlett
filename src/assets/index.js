6c99e21c37f2f6d6ee8b9d2baf55869a5a69774f
seanleecodes-code <seanlee.codes@gmail.com>
2025-11-30 22:06:19 -0500

    fixed the bug that wrong the path

--- src/assets/index.js
+++ src/assets/index.js
@@ -24,7 +24,6 @@ import metricwebapp from './projects/metricswebapp.png';
 import spacetravelers from './projects/spacetravelers.png';
 import recipeapp from './projects/recipeapp.png';
 import trackbudget from './projects/trackbudget.png';
-import carrental from './projects/carrental.png';
 
 // carousel img
 import guesstheword1 from './carousels/guess the word screenshot 1.png';
